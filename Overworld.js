e54e7231fe1f12790e11da44dc24c9d669b5dae3
Johan Hammarstedt <johan.hammarstedt@gmail.com>
2024-03-03 15:45:33 +0100

    adding first transition

--- Overworld.js
+++ Overworld.js
@@ -41,27 +41,50 @@ class Overworld {
      }
      step();
   }
- 
+  bindActionInput() {
+    new KeyPressListener("Enter", () => {
+      //is there a person here to talk to?
+      this.map.checkForActionCutscene()
+    
+    })
+  }
+  
+  bindHeroPositionCheck() {
+    document.addEventListener("PersonWalkingComplete", e => {
+      if (e.detail.whoId === "hero") {
+        this.map.checkForFootstepCutscene()
+      }
+    })
+   }
+
+  startMap(mapConfig){
+    this.map = new OverworldMap(mapConfig);
+    this.map.overworld = this;
+    this.map.mountObjects();
+
+  }
   init() {
-   this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
-   this.map.mountObjects();
- 
+  this.startMap(window.OverworldMaps.DemoRoom);
+
+   this.bindActionInput()
+   this.bindHeroPositionCheck();
+   
    this.directionInput = new DirectionInput();
    this.directionInput.init();
  
    this.startGameLoop();
  
-   this.map.startCutscene([
-    { who: "hero", type: "walk",  direction: "down" },
-    { who: "hero", type: "walk",  direction: "down" },
-    { who: "npcA", type: "walk",  direction: "up" },
-    { who: "npcA", type: "walk",  direction: "left" },
-    { who: "hero", type: "stand",  direction: "right",time:200},
-    { type: "textMessage", text: "Well Hello there!"}
-    //  { who: "npcA", type: "walk",  direction: "left" },
-    //  { who: "npcA", type: "walk",  direction: "left" },
-    //  { who: "npcA", type: "stand",  direction: "up", time: 800 },
-   ])
+  //  this.map.startCutscene([
+  //   { who: "hero", type: "walk",  direction: "down" },
+  //   { who: "hero", type: "walk",  direction: "down" },
+  //   { who: "npcA", type: "walk",  direction: "up" },
+  //   { who: "npcA", type: "walk",  direction: "left" },
+  //   { who: "hero", type: "stand",  direction: "right",time:200},
+  //   { type: "textMessage", text: "Well Hello there!"}
+  //   //  { who: "npcA", type: "walk",  direction: "left" },
+  //   //  { who: "npcA", type: "walk",  direction: "left" },
+  //   //  { who: "npcA", type: "stand",  direction: "up", time: 800 },
+  //  ])
  
   }
  }
